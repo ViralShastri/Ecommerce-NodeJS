befe5b4b0d4af39396fcef5d85a6058215a99a3b
Viral Shsatri <shastriv19@gmail.com>
2020-10-16 18:00:55 +0530

    Sql Database Intigration

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 const app = express();
 
@@ -21,6 +22,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen((port = 3500), (hostname = "localhost"), () => {
+app.listen((port = 3500), (hostname = "127.0.0.1"), () => {
     console.log(`Server running at http://${hostname}:${port}`);
 });
